4ede8042e1fb44188a46fe7fd7ada70ea865ef62
Mariusz Fik <mariusz@fidano.pl>
2017-03-03 23:51:32 +0100

    Update infobox widgets in dashboard main view.
    Show real counters for users and events. More to come.
    
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
@@ -18,6 +19,7 @@ use Encore\Admin\Widgets\Collapse;
 use Encore\Admin\Widgets\InfoBox;
 use Encore\Admin\Widgets\Tab;
 use Encore\Admin\Widgets\Table;
+use Encore\Admin\Auth\Database\Administrator;
 
 class HomeController extends Controller
 {
@@ -29,10 +31,10 @@ class HomeController extends Controller
             $content->description('Description...');
 
             $content->row(function ($row) {
-                $row->column(3, new InfoBox('New Users', 'users', 'aqua', '/admin/users', '1024'));
+                $row->column(3, new InfoBox('Users', 'users', 'aqua', '/admin/auth/users', Administrator::all('id')->count()));
                 $row->column(3, new InfoBox('New Orders', 'shopping-cart', 'green', '/admin/orders', '150%'));
                 $row->column(3, new InfoBox('Articles', 'book', 'yellow', '/admin/articles', '2786'));
-                $row->column(3, new InfoBox('Documents', 'file', 'red', '/admin/files', '698726'));
+                $row->column(3, new InfoBox('Events', 'calendar-o', 'red', '/admin/events', Event::all('id')->count()));
             });
 
             $content->row(function (Row $row) {
